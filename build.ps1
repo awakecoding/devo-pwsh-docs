c82318927c9bb573cf71dc325d5b216d874e12d4
Marc-André Moreau <mamoreau@devolutions.net>
2024-11-07 14:28:53 -0500

    generation functional docs site

---
+++ build.ps1
@@ -0,0 +1,43 @@
+
+$OutputFolder = "./content/docs"
+$ModuleName = "Devolutions.PowerShell"
+
+Remove-Item $OutputFolder -Recurse -Force | Out-Null
+
+Import-Module $ModuleName
+
+Get-Command -Module $ModuleName -CommandType Cmdlet | ForEach-Object {
+	$CommandHelp = New-CommandHelp $_
+    #$CommandHelp.Metadata['date'] = (Get-Date $CommandHelp.Metadata['ms.date'] -Format 'yyyy-MM-dd').ToString()
+    $CommandHelp.Metadata['date'] = (Get-Date -Format 'yyyy-MM-dd').ToString()
+    $CommandHelp.RelatedLinks | ForEach-Object { $_.Uri = "/docs/$($_.LinkText)" }
+    #$CommandHelp.RelatedLinks | ForEach-Object { $_.Uri = "@/docs/$($_.LinkText).md" }
+    #$CommandHelp.RelatedLinks += [PSCustomObject]@{ Uri = $ModuleName; LinkText = $ModuleName }
+    $CommandHelp.Inputs | Where-Object { $_.Description.Contains("{{ Fill") } | ForEach-Object { $_.Description = "" }
+    $CommandHelp.Outputs | Where-Object { $_.Description.Contains("{{ Fill") } | ForEach-Object { $_.Description = "" }
+    if ($CommandHelp.Synopsis -and $CommandHelp.Synopsis.Contains('Fill ')) {
+        $CommandHelp.Synopsis = ""
+    }
+	Export-MarkdownCommandHelp -CommandHelp $CommandHelp -OutputFolder $OutputFolder -Force
+}
+
+Get-Item "$OutputFolder/$ModuleName/*.md" | ForEach-Object {
+    $filePath = $_.FullName
+    $content = Get-Content -Path $filePath -Raw
+    $content = $content -replace '\{\{Insert list of aliases\}\}', ''
+    $content = $content -replace '\{\{ Fill in the related links here \}\}', ''
+    Set-Content -Path $filePath -Value $content
+}
+
+$content = @"
++++
+title = "PowerShell cmdlets"
+sort_by = "title"
+template = "cmdlet.html"
+page_template = "cmdlet-page.html"
++++
+"@
+Set-Content -Path "$OutputFolder/$ModuleName/_index.md" -Value $content -Force
+
+Move-Item ".\$OutputFolder\$ModuleName\*" $OutputFolder
+Remove-Item ".\$OutputFolder\$ModuleName"
